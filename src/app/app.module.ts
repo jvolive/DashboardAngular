63fca2be2625a81ee6d91d1bf1e852e4c96efbab
jvolive <joaooliveira1999@outlook.com>
2023-07-12 01:30:22 -0300

    Changes in server and shared folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,23 @@ import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgChartsModule } from 'ng2-charts';
 
+//components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { SectionSalesComponent } from './sections/section-sales/section-sales.component';
 import { SectionOrdersComponent } from './sections/section-orders/section-orders.component';
-import { SectionHealthComponent } from './sections/section-health/section-health.component';
 import { appRoutes } from 'src/routes';
 import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
 import { LineChartComponent } from './charts/line-chart/line-chart.component';
 import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
 import { PaginationComponent } from './pagination/pagination.component';
+import { ServerComponent } from './server/server.component';
+
+//services
+import { SalesDataService } from './services/sales-data.service';
+import { ServerService } from './services/server.service';
 
 @NgModule({
   declarations: [
@@ -23,11 +28,11 @@ import { PaginationComponent } from './pagination/pagination.component';
     SidebarComponent,
     SectionSalesComponent,
     SectionOrdersComponent,
-    SectionHealthComponent,
     BarChartComponent,
     LineChartComponent,
     PieChartComponent,
     PaginationComponent,
+    ServerComponent,
   ],
   imports: [
     BrowserModule,
