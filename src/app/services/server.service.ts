69bb42842ae6ef1f4c776301b0e09612a54e3124
jvolive <joaooliveira1999@outlook.com>
2023-07-14 12:06:13 -0300

    fixed the bug in server.services

--- src/app/services/server.service.ts
+++ src/app/services/server.service.ts
@@ -1,46 +1,46 @@
 import { Injectable } from '@angular/core';
-import { Http, RequestOptions, Response, Headers } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { ServerMessage } from '../shared/server-message';
 import { Server } from '../shared/server';
-import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class ServerService {
-  // https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.4
+  constructor(private _http: HttpClient) {}
 
-  constructor(private _http: Http) {
-    this.headers = new Headers({
+  getServers(): Observable<Server[]> {
+    const url = 'http://localhost:5000/api/server';
+    const headers = new HttpHeaders({
       'Content-Type': 'application/json',
       Accept: 'q=0.8;application/json;q=0.9',
     });
 
-    this.options = new RequestOptions({ headers: this.headers });
-  }
-
-  options: RequestOptions;
-  headers: Headers;
-
-  getServers(): Observable<Server[]> {
-    return this._http
-      .get('http://localhost:5000/api/server')
-      .map((res) => res.json())
-      .catch(this.handleError);
+    return this._http.get<Server[]>(url, { headers }).pipe(
+      map((res) => res),
+      catchError(this.handleError)
+    );
   }
 
   handleError(error: any) {
-    const errMsg = error.message
-      ? error.message
-      : error.status
-      ? `${error.status} - ${error.statusText}`
-      : 'Server error';
+    const errMsg =
+      error.message ||
+      (error.status ? `${error.status} - ${error.statusText}` : 'Server error');
 
     console.log(errMsg);
-    return Observable.throw(errMsg);
+    return throwError(errMsg);
   }
 
-  handleServerMessage(msg: ServerMessage): Observable<Response> {
-    const url = 'http://localhost:5000/api/server/' + msg.id;
-    return this._http.put(url, msg, this.options).map((res) => res.json());
+  handleServerMessage(msg: ServerMessage): Observable<any> {
+    const url = `http://localhost:5000/api/server/${msg.id}`;
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Accept: 'q=0.8;application/json;q=0.9',
+    });
+
+    return this._http.put(url, msg, { headers }).pipe(
+      map((res) => res),
+      catchError(this.handleError)
+    );
   }
 }
